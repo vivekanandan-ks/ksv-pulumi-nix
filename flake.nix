924031cd7c65ff18eb1b00ba295c50ace734c8be
vivekanandan-ks <ksvdevksv@gmail.com>
2025-05-21 12:23:24 +0530

    minor changes

--- flake.nix
+++ flake.nix
@@ -54,7 +54,7 @@
             exec ${pkgs.fish}/bin/fish --init-command '
             function fish_prompt
                 set_color blue
-                echo -n "nix-shell"
+                echo -n "nix-shell🐠🐚 "
                 set_color normal
                 echo -n "🐠🐚 "
             end'
