f2060e9ff48a5974fa7b516c8f410cf08deed103
vivekanandan-ks <ksvdevksv@gmail.com>
2025-05-21 18:06:55 +0530

    Refactored the flake and some changes

--- flakeold.nix
+++ flakeold.nix
@@ -8,20 +8,43 @@
       url = "github:hercules-ci/gitignore.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    pip2nix.url = "github:nix-community/pip2nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, gitignore, ... }:
+  outputs = { self, nixpkgs, flake-utils, gitignore, pip2nix, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         inherit (gitignore.lib) gitignoreSource;
         pkgs = import nixpkgs { 
           inherit system;
         };
+        packageOverrides = pkgs.callPackage (gitignoreSource ./python-packages.nix) {};
+
+        python = pkgs.python313.override { inherit packageOverrides; };
+        pythonEnv = python.withPackages (p: with p; [
+            #from pypi (add only in requirements.txt)
+            import (gitignoreSource ./requirements.nix)
+            
+            #from nixpkgs (add here below)
+        ]);
+        
+        install-requirements = pkgs.writeShellApplication {
+          name = "install-requirements";
+          runtimeInputs = [pip2nix.defaultPackage.${system}];
+          text = ''
+            #!/usr/bin/env ${pkgs.bash}/bin/bash
+            echo "Running install-requirements..."
+            ${pip2nix.defaultPackage.${system}}/bin/pip2nix generate -r ./requirements.nix
+          '';
+        };
 
       in {
 
+        packages.install-requirements = install-requirements;
+
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
+            pythonEnv
             pulumi
             pulumi-bin
             pulumiPackages.pulumi-python
@@ -30,6 +53,7 @@
             
           ];
           shellHook = ''
+            ${install-requirements}/bin/install-requirements
             echo "welcome to the pulumi shell created by https://github.com/vivekanandan-ks" | ${pkgs.cowsay}/bin/cowsay
             #my custom fish shel prompt customized (comment below to use defualt bash)
             exec ${pkgs.fish}/bin/fish --init-command '
